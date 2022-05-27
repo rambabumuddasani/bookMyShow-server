53c31e656c6d08ae2f1fb9adcfa6ce747627f8bb
ram <rambabu0006@gmail.com>
2022-05-27 19:45:44 +0530

    bookmyshow server impl

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO book_my_show (booking_id,user_name,show_name,booking_time,user_count,price) VALUES (1,'ram','1st',CURRENT_DATE(),200,100);
+INSERT INTO book_my_show (booking_id,user_name,show_name,booking_time,user_count,price) VALUES (2,'mouni','1st',CURRENT_DATE(),200,100);
